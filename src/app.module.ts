a165b88991f9fd1daa9835f87e89470a30158adb
Adrian Ponce <adrian.ponce.89@gmail.com>
2023-12-13 12:21:44 -0300

    Add validation for enviroment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,12 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { EvnConfig } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EvnConfig],
+      validationSchema: JoiValidationSchema,
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
